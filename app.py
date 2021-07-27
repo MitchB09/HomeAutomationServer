36e9a8550dc61486638049bec850f6540631ab27
Mitch Bilensky <mitchbilensky@gmail.com>
2021-07-26 21:57:13 -0300

    Servo updates

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, escape
 from flask_cors import CORS
+import RPi.GPIO as GPIO
+from time import sleep
+
+servoPIN = 17
+GPIO.setMode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+pwm = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM @ 50hz
+pwm.start(0)  # Initialization
 
 app = Flask(__name__)
 CORS(app)
+
+
+def SetAngle(angle):
+    duty = angle/18 + 2
+    GPIO.output(servoPIN, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(servoPIN, False)
+    pwm.ChangeDutyCycle(0)
 
 
 @app.route('/')
@@ -16,13 +34,13 @@ def user(user):
 
 
 @app.route('/bootprinter', methods=['GET'])
-def bootpc_status():
+def bootprinter_status():
     print("Check things aren't broken....")
     return {'status': 'OK'}
 
 
 @app.route('/bootprinter', methods=['POST'])
-def bootpc_execute():
+def bootprinter_execute():
     print("Boot Printer servo code....")
     return ('', 204)
 
@@ -36,4 +54,8 @@ def bootpc_status():
 @app.route('/bootpc', methods=['POST'])
 def bootpc_execute():
     print("Boot PC servo code....")
+    SetAngle(90)
+    sleep(2)
+    SetAngle(0)
+    sleep(2)
     return ('', 204)
